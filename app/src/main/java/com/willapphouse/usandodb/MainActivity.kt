69dc15ed2fad180ba00cfe3c7297147556a3e64f
William <wffavarin@gmail.com>
2026-06-22 17:23:12 -0300

    Projeto com código kotlin e interface XML sem usar sqlite.

--- app/src/main/java/com/willapphouse/usandodb/MainActivity.kt
+++ app/src/main/java/com/willapphouse/usandodb/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.willapphouse.usandodb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btIncluir.setOnClickListener {  }
+        binding.btAlterar.setOnClickListener {  }
+        binding.btExcluir.setOnClickListener {  }
+        binding.btPesquisar.setOnClickListener {  }
+        binding.btListar.setOnClickListener {  }
     }
 }
